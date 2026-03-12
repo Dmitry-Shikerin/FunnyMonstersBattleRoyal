380f711d0d5592a9cbe19cbdd586bb6c2daedb36
Dmitry_Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2026-03-12 20:47:57 +0300

    Add assets

---
+++ Assets/MyAssets/HYPEPOLY - Battle Royale Show 2/Scripts/WNDRigidbodyCenter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WNDRigidbodyCenter : MonoBehaviour
+{
+    public Vector3 CenterOfMass;
+    void Awake()
+    {
+        GetComponent<Rigidbody>().centerOfMass = CenterOfMass;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color32(0, 255, 0, 50);
+        Gizmos.DrawSphere(transform.TransformPoint(CenterOfMass), 0.5f);
+    }
+}
