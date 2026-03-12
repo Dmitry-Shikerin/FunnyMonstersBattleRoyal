13e22e162c9c088b1ade2794fab7f0e0ecaa2aba
Dmitry_Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2026-03-12 21:51:06 +0300

    integrate plugins

---
+++ Assets/Plugins/LeoEcsProtoCs/Leopotam.EcsProto.Unity/Runtime/EntityLink.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Leopotam.EcsProto.Unity.Plugins.LeoEcsProtoCs.Leopotam.EcsProto.Unity.Runtime
+{
+    public class EntityLink : MonoBehaviour
+    {
+        [Header("Entity")] 
+        [SerializeField] private List<EntityModule> _modules;
+        public ProtoEntity Entity { get; private set; }
+        public ProtoWorld World { get; private set; }
+
+        public void Initialize(ProtoEntity entity, ProtoWorld world)
+        {
+            Entity = entity;
+            World = world;
+            
+            foreach (EntityModule module in _modules)
+                module.Initialize(entity, world, this);
+        }
+        
+        public T GetModule<T>()
+            where T : EntityModule
+        {
+            foreach (EntityModule module in _modules)
+            {
+                if (module is T concrete)
+                    return concrete;
+            }
+            
+            throw new InvalidOperationException("Module not found");
+        }
+        
+        public bool TryGetModule<T>(out T findedModule)
+            where T : EntityModule
+        {
+            foreach (EntityModule module in _modules)
+            {
+                if (module is T concrete)
+                {
+                    findedModule = concrete;
+                    return true;
+                }
+            }
+
+            findedModule = null;
+            
+            return false;
+        }
+
+        public IReadOnlyList<EntityModule> GetModules() =>
+            _modules;
+
+        [Button]
+        private void FillModules()
+        {
+            EntityModule[] modules = gameObject.GetComponents<EntityModule>();
+            AddModules(modules);
+            modules = gameObject.GetComponentsInChildren<EntityModule>();
+            AddModules(modules);
+        }
+
+        private void AddModules(EntityModule[] modules)
+        {
+            foreach (EntityModule module in modules)
+            {
+                if (_modules.Contains(module))
+                    continue;
+                
+                _modules.Add(module);
+            }
+        }
+
+        public EntityLink Show()
+        {
+            gameObject.SetActive(true);
+            return this;
+        }
+
+        public EntityLink Hide()
+        {
+            gameObject.SetActive(false);
+            return this;
+        }
+    }
+}
---
+++ Assets/Plugins/LeoEcsProtoCs/Leopotam.EcsProto.Unity/Runtime/EntityModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Leopotam.EcsProto.Unity.Plugins.LeoEcsProtoCs.Leopotam.EcsProto.Unity.Runtime
+{
+    public class EntityModule : MonoBehaviour
+    {
+        public ProtoEntity Entity { get; private set; }
+        protected ProtoWorld World { get; private set; }
+        public EntityLink Link { get; private set; }
+        protected List<IDisposable> Disposables { get; private set; } = new ();
+        protected bool IsInitialized { get; private set; }
+
+        public void Initialize(
+            ProtoEntity entity, 
+            ProtoWorld world,
+            EntityLink link)
+        {
+            Entity = entity;
+            World = world;
+            Link = link;
+            IsInitialized = true;
+        }
+
+        private void OnDisable()
+        {
+            foreach (IDisposable disposable in Disposables)
+                disposable?.Dispose();
+            
+            OnAfterDisable();
+        }
+
+        protected virtual void OnAfterDisable()
+        {
+        }
+    }
+}
