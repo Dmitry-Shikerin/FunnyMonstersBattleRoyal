fb1798bccde0d75efbc27a970ebfb96c2e6259a5
Dmitry_Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2026-07-20 18:26:31 +0300

    add logic

---
+++ Assets/Sources/BoundedContexts/Networks/FusionStartup.cs
@@ -0,0 +1,38 @@
+﻿using Fusion;
+using Fusion.Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Sources.BoundedContexts.Networks
+{
+    public class FusionStartup : MonoBehaviour
+    {
+        [SerializeField] private NetworkSceneManagerDefault _sceneManager;
+        [SerializeField] private NetworkRunner _runner;
+        [SerializeField] private string _sessionName = "SampleSession";
+
+        private void Start()
+        {
+            StartSimulation(GameMode.AutoHostOrClient);
+        }
+
+        private async void StartSimulation(GameMode gameMode)
+        {
+            _runner.ProvideInput = true;
+            
+            await _runner.StartGame(new StartGameArgs
+            {
+                GameMode = gameMode,
+                SceneManager = _sceneManager,
+                Scene = GetScene(),
+                SessionName = _sessionName,
+            });
+        }
+
+        private SceneRef GetScene()
+        {
+            int buildIndex = SceneManager.GetActiveScene().buildIndex;
+            return SceneRef.FromIndex(buildIndex);
+        }
+    }
+}
