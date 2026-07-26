f892c9b78f2091c04d2c1898c1302a279a0739a8
Dmitry_Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2026-07-26 21:02:41 +0300

    change main menu ui buttons

---
+++ Assets/Sources/BoundedContexts/Hud/Controllers/MainMenu/NameInputUiView.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using ParadoxNotion.Design;
+using Sources.EcsBoundedContexts.Common.Domain.Constants;
+using Sources.Frameworks.DeepFramework.DeepUiManager.Controllers.Implementation.States;
+using Sources.Frameworks.DeepFramework.DeepUiManager.Domain.Enums;
+
+namespace Sources.BoundedContexts.Hud.Controllers.MainMenu
+{
+    [Category(NcCategoriesConst.Ui)]
+    public class NameInputUiView : UiViewStateBase
+    {
+        protected override bool IsHideAllViewsEntered { get; } = true;
+        protected override bool IsHideAllViewsExited { get; }
+        
+        protected override void AddEnterShowedViews(List<UiViewId> viewIds)
+        {
+            viewIds.Add(UiViewId.NameInput);
+            viewIds.Add(UiViewId.HudBackground);
+        }
+
+        protected override void AddEnterHidedViews(List<UiViewId> viewIds)
+        {
+        }
+
+        protected override void AddExitShowViews(List<UiViewId> viewIds)
+        {
+        }
+
+        protected override void AddExitHidedViews(List<UiViewId> viewIds)
+        {
+        }
+
+        protected override void AddEnterUiActions(List<UiActionId> uiActionIds)
+        {
+        }
+
+        protected override void AddExitUiActions(List<UiActionId> uiActionIds)
+        {
+        }
+    }
+}
