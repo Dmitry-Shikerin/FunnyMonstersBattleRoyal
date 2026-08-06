01cae34b6a7f217f0e6f3d48fd0fb7dc4cf0fe99
Dmitry_Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2026-08-06 12:28:55 +0300

    change ui settings toggles

--- Assets/Sources/EcsBoundedContexts/Settings/Presentation/FullScreenSettingsModule.cs
+++ Assets/Sources/EcsBoundedContexts/Settings/Presentation/FullScreenSettingsModule.cs
@@ -1,9 +1,12 @@
 ﻿using Leopotam.EcsProto.Unity.Plugins.LeoEcsProtoCs.Leopotam.EcsProto.Unity.Runtime;
+using Sirenix.OdinInspector;
+using Sources.Frameworks.DeepFramework.DeepUiManager.Presentation.Implementation.Buttons;
+using UnityEngine;
 
 namespace Sources.EcsBoundedContexts.Settings.Presentation
 {
     public class FullScreenSettingsModule : EntityModule
     {
-        
+        [Required] [SerializeField] private UiToggle _toggle;
     }
 }
