f16c0dcf6fca29cd3c1f398f67b2dead05ffaf0d
Dmitry_Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2026-08-20 15:04:45 +0300

    fix main menu ui view

--- Assets/Sources/BoundedContexts/Hud/Presentations/MainMenu/MainMenuUiView.cs
+++ Assets/Sources/BoundedContexts/Hud/Presentations/MainMenu/MainMenuUiView.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using Fusion.Menu;
-using Leopotam.EcsProto.Unity.Plugins.LeoEcsProtoCs.Leopotam.EcsProto.Unity.Runtime;
 using Reflex.Attributes;
 using Sirenix.OdinInspector;
 using Sources.BoundedContexts.NetworkCore.Services;
+using Sources.BoundedContexts.Players.Presentation.Ui;
 using Sources.Frameworks.DeepFramework.DeepUiManager.Presentation.Implementation.Buttons;
 using Sources.Frameworks.DeepFramework.DeepUiManager.Presentation.Implementation.Views;
 using UnityEngine;
@@ -13,7 +13,7 @@ namespace Sources.BoundedContexts.Hud.Presentations.MainMenu
     public class MainMenuUiView : UiView
     {
         [Required] [SerializeField] private UiButton _quickGameButton;
-        [field: Required] [field: SerializeField] public EntityLink PlayerNameUiLink { get; private set; }
+        [field: Required] [field: SerializeField] public GameplayPlayerNameUiView PlayerNameUiView { get; private set; }
         [field: Required] [field: SerializeField] public FusionMenuConnectArgs ConnectArgs { get; private set; }
 
         private NetworkStartGameService _startGameService;
@@ -24,14 +24,14 @@ namespace Sources.BoundedContexts.Hud.Presentations.MainMenu
         private void Construct(NetworkStartGameService startGameService) =>
             _startGameService = startGameService;
 
-        public void EnableQuickGameButton() =>
-            _quickGameButton.Interactable(true);
-
         private void OnEnable() =>
             _quickGameButton.OnClick += StartQuickGame;
 
         private void OnDisable() =>
             _quickGameButton.OnClick -= StartQuickGame;
+
+        public void EnableQuickGameButton() =>
+            _quickGameButton.Interactable(true);
 
         private async void StartQuickGame()
         {
